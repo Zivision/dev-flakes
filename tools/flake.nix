ab477446f40f4b14931deea2f306e87a184391fd
Zivision <zcastro703@gmail.com>
2026-04-03 16:10:05 -0500

    feat: tools flake.nix

---
+++ tools/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Tool Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          kubectl
+          terraform
+          google-cloud-sdk
+        ];
+        shellHook = ''
+          echo "Tools Ready!"
+        '';
+      };
+    };
+}
