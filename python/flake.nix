70bf478b5c8d1c64db419c72cc02f162b7bec950
Zivision <zcastro703@gmail.com>
2026-04-03 14:21:38 -0500

    feat: python flake.nix

---
+++ python/flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Python Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          # Basic Dependancies
+          python313Packages.pip
+          python313Packages.virtualenv
+
+          # For my text editor (Emacs)
+          pyright
+          black
+          python313Packages.pyflakes
+          python313Packages.isort
+          python313Packages.pytest
+
+          # Python itself
+          python313
+
+          # Python Project Dependancies
+          # python313Packages.requests for example
+
+        ];
+        shellHook = ''
+          echo "Python Started!"
+        '';
+      };
+    };
+}
